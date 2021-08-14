566fc2bc79b88fa9ff7b70111f3ab89af8fe91c7
Elbastito <naitsabes_ochoa@hotmail.com>
2021-08-13 20:42:45 -0500

    primer ejercicio

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 console.log("Hola mundo");
 
 alert("hola mundo");
+var edad= 30;
 
+if(edad>=18&&edad<=30){
+    console.log("puedes votar");
+    if(edad===30){
+        console.log("felicidades");
+    }
+}
+else{
+    console.log("perdon, tu no tienes derecho a votar");
 
+}
