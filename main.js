e13d8183e95bf91bfbe34ab5dcb5cf2f79bf1d6f
Elbastito <naitsabes_ochoa@hotmail.com>
2021-08-31 22:54:39 -0500

    Ejercicio final de programacion funcional

--- main.js
+++ main.js
@@ -1,5 +1,44 @@
-console.log("Hola mundo");
+// Ejercicio 1
+/* function checa(a,b){
+    let i=0;
+    let nomF=[];
+    for(i=0;i<b.length;i=i+1){
+        if(b[i].includes(a)){
+            nomF.push(b[i]);
+        }
+    }
+    return nomF;
+}
 
-alert("hola mundo");
+var nom=[],nommodi=[];
+let l=0;
 
+l=prompt("Cuantos nombres quieres introducir");
+for(i=0;i<l;i=i+1){
+    nom[i]=prompt("Dame le nombre");
+}
+let letra=prompt("Dime la letra que quieres buscar");
+nommodi=checa(letra,nom);
+console.log("Original",nom);
+console.log("Filtrado",nommodi); */
 
+/* 
+function greaterThan(n) {
+    return m => m > n;
+  }
+  let greaterThan7 = greaterThan(7);
+  // 6 => 6 > 7
+  console.log(greaterThan7(6));
+  // → true 
+*/
+
+// Ejercicio Filter
+let nombres=[];
+let checa="";
+for(let i=0;i<3;i=i+1){
+    nombres[i]=prompt("Dame un nombre");
+}
+
+let nom3=nombres.filter((n)=>n.length%2===0|| n[3]==="a"||n[3]==="e"||n[3]==="i"||n[3]==="o"||n[3]==="u");
+
+console.log(nom3);
